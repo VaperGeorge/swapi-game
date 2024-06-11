0247dd35a9cdab589f3c8c5feda7e221bd585dd0
George Vorobyov <george@oyenetwork.com>
2024-06-11 14:40:05 +0200

    add starships service

---
+++ src/shared/services/starships.service.ts
@@ -0,0 +1,38 @@
+import { Injectable, inject } from '@angular/core';
+import { BehaviorSubject, EMPTY, Observable, catchError, map, tap } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+import { ResourceListItem, ResourceListResponse } from '../interfaces';
+import { API_URL } from '../constants/constants';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StarShipsService {
+  private readonly http = inject(HttpClient);
+
+  private starShips$ = new BehaviorSubject<ResourceListItem[]>([]);
+
+  get starShips(): BehaviorSubject<ResourceListItem[]> {
+    return this.starShips$;
+  }
+
+  set starShips(value: ResourceListItem[]) {
+    this.starShips$.next(value);
+  }
+
+  getStarShips(): Observable<ResourceListItem[]> {
+    return this.http.get<ResourceListResponse>(`${API_URL}/starships`).pipe(
+      tap((response) => (this.starShips = response.results)),
+      map((response) => response.results),
+      catchError((error) => {
+        console.error(error);
+        return EMPTY;
+      }),
+    );
+  }
+
+  clear(): void {
+    this.starShips = [];
+  }
+}
