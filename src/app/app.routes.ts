88ed37138b64c9f71a1dce1b7b1980800ef082d1
George Vorobyov <george@oyenetwork.com>
2024-06-11 10:19:56 +0200

    add lazy loading routers with redirection

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { GameStep } from '../shared';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: `${GameStep.START}`,
+    loadComponent: () =>
+      import('./start/start.component').then((mod) => mod.StartComponent),
+  },
+  { path: '', redirectTo: `${GameStep.START}`, pathMatch: 'full' },
+];
