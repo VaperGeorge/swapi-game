92678b1f3b46516fffd564efbf5d8ebf650ac33c
George Vorobyov <george@oyenetwork.com>
2024-06-11 11:14:07 +0200

    implement start component logic

--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
+
+import { GameStep, StepService } from '../../shared';
 
 @Component({
   selector: 'app-start',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatIconModule],
   templateUrl: './start.component.html',
-  styleUrl: './start.component.scss'
+  styleUrl: './start.component.scss',
 })
-export class StartComponent {
+export class StartComponent implements OnInit {
+  private readonly router = inject(Router);
+  private readonly stepService = inject(StepService);
 
+  ngOnInit(): void {
+    this.stepService.currentStep = GameStep.START;
+  }
+
+  nextStep() {
+    this.router.navigate([`${GameStep.SELECTION}`]);
+    this.stepService.currentStep = GameStep.SELECTION;
+  }
 }
