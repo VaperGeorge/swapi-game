5c284fa6d6f3adf5417c7d2b1b469b56754bb5c9
George Vorobyov <george@oyenetwork.com>
2024-06-11 14:39:32 +0200

    generate info-card component & add logic

--- src/app/info-card/info-card.component.spec.ts
+++ src/app/info-card/info-card.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { StartComponent } from './start.component';
+import { InfoCardComponent } from './info-card.component';
 
-describe('StartComponent', () => {
-  let component: StartComponent;
-  let fixture: ComponentFixture<StartComponent>;
+describe('InfoCardComponent', () => {
+  let component: InfoCardComponent;
+  let fixture: ComponentFixture<InfoCardComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [StartComponent]
+      imports: [InfoCardComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(StartComponent);
+    fixture = TestBed.createComponent(InfoCardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ src/app/info-card/info-card.component.ts
@@ -0,0 +1,37 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  inject,
+} from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { AsyncPipe, NgClass, TitleCasePipe } from '@angular/common';
+
+import { PersonInfo, Resource, ResourceService, StarShip } from '../../shared';
+
+@Component({
+  selector: 'app-info-card',
+  standalone: true,
+  imports: [MatCardModule, TitleCasePipe, AsyncPipe, NgClass],
+  templateUrl: './info-card.component.html',
+  styleUrl: './info-card.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class InfoCardComponent {
+  private readonly resourceService = inject(ResourceService);
+
+  @Input() set data(value: PersonInfo | StarShip) {
+    this.resource$.value === Resource.PEOPLE
+      ? (this.person = value as PersonInfo)
+      : (this.starShip = value as StarShip);
+  }
+
+  @Input() isWin = false;
+
+  resource$ = this.resourceService.currentResource;
+
+  Resource = Resource;
+
+  starShip!: StarShip;
+  person!: PersonInfo;
+}
